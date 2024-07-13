97d3e49dca5d7bfa033d5775952064932357726b
Adriano G <adrianogomesfilho98@gmail.com>
2024-07-13 13:50:10 -0300

    fazendo requests em site real

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
+# first >  pip install requests
+import requests
 
-with open('home.html', 'r') as html_file: #open the file with "r" read mode, #with open and closes
-    content = html_file.read()
-
-    soup = BeautifulSoup(content, 'lxml')
-    course_cards = soup.find_all('div', class_='card')
-    for course in course_cards:
-        course_name = course.h5.text
-        course_price = course.a.text.split()[-1] #split the words by blank space and return the last (-1)
-
-        print(f'{course_name} costs {course_price}')
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35').text
+print(html_text)
